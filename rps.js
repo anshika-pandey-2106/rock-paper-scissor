de99b56076fba9beb8da4094812ecc9820513da2
Anshika Pandey <anshika.p2106@gmail.com>
2024-08-01 23:43:13 +0530

    function to play a round

--- rps.js
+++ rps.js
@@ -1,4 +1,7 @@
 //console.log("hello");
+
+let humanScore=0, computerScore=0;
+
 function getComputerChoice(){
     let random=Math.floor(Math.random()*3);
     let computerChoice;
@@ -18,4 +21,42 @@ function getHumanChoice(){
     return humanChoice.toLowerCase();
 }
 
+
+function playRound(humanChoice, computerChoice)
+ {
+    if(humanChoice==computerChoice)
+    {
+        console.log("it's a tie!");
+    }
+    else if(humanChoice=="rock" && computerChoice=="scissors" ){
+        console.log("Rock beats Scissors! You Win!");
+        humanScore+=1;
+    }
+    else if(humanChoice=="paper" && computerChoice=="rock"){
+        console.log("Paper beats Rock! You Win!");
+        humanScore+=1;
+
+    }
+    else if(humanChoice=="scissors" && computerChoice=="paper" ){
+        console.log("Scissors beat Paper! You Win!");
+        humanScore+=1;
+    }
+    else if(humanChoice=="paper" && computerChoice=="scissors" ){
+        console.log("Scissors beat Paper! You Lose!");
+        computerScore+=1;
+        
+    }
+    else if(humanChoice=="scissors" && computerChoice=="rock" ){
+        console.log("Rock beats Scissors! You lose!");
+        computerScore+=1;
+    }
+    else if(humanChoice=="rock" && computerChoice=="paper"){
+        console.log("Paper beats Rock! You Lose!");
+        computerScore+=1;
+
+    }
+
+    
+
+ }
 
